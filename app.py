4a0fdba380f422233d2c196ca7e3d111297f3bd1
kisumutalent <homiemusa@gmail.com>
2018-05-03 08:58:07 +0300

    [Feature  #157273303] user authentication

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, redirect, url_for, request
-
+from flask import Flask, render_template, redirect, url_for, request, session,flash
+from functools import wraps
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return "Hello refuge wise"
+app.secret_key  = "my precious"
 
+def login_required(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
+
+@app.route('/')
+@login_required
+def home():
+	return render_template('index.html')
 
 @app.route('/welcome')
+@login_required
 def welcome():
 	return render_template('welcome.html')
 
@@ -21,8 +34,16 @@ def login():
 		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
 			error = 'Invalid credentials. Please try agian.'
 		else:
+			session['logged_in'] = True
+			flash('You were just logged_in!')
 			return redirect(url_for('home'))
 	return render_template('login.html', error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+	session.pop('logged_in', None)
+	return redirect(url_for('welcome'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
