fb201209d4364d628fb953116e540f0b0553900b
kisumutalent <homiemusa@gmail.com>
2018-05-08 10:27:19 +0300

    [Feature #157385149] set up sqlite3 db

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, redirect, url_for, request, session,flash
+from flask import Flask, render_template, redirect, url_for, request, session,flash,g
 from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
 
+
 app.secret_key  = "my precious"
+app.database = "sample.db"
 
 def login_required(f):
 	@wraps(f)
@@ -17,7 +20,11 @@ def login_required(f):
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	g.db = connect_db()
+	cur = g.db.execute('select * from posts')
+	posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('index.html', posts=posts)
 
 @app.route('/welcome')
 @login_required
@@ -43,6 +50,12 @@ def login():
 def logout():
 	session.pop('logged_in', None)
 	return redirect(url_for('welcome'))
+
+
+
+def connect_db():
+	return sqlite3.connect(app.database)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
