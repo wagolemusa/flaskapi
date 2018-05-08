616c90733a2f2f737d3a4da03503a070acfc0e14
kisumutalent <homiemusa@gmail.com>
2018-05-08 10:43:40 +0300

    modifing app.py on query the db

--- app.py
+++ app.py
@@ -19,10 +19,16 @@ def login_required(f):
 	return wrap
 
 @app.route('/')
+@login_required
 def home():
 	g.db = connect_db()
 	cur = g.db.execute('select * from posts')
-	posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+	posts = []
+	for row in cur.fetchall():
+		posts.append(dict(title=row[0], description=row[1]))
+
+	#posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+
 	g.db.close()
 	return render_template('index.html', posts=posts)
 
